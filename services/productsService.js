9bc65f66e3295d7f2c293590efb70f50d59d63a8
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-01 20:44:55 -0300

    feat: :sparkles: Adds <productById> to Service layer
    Adds conditional to check if the URL came with and id on its params, if so, redirects to Model layer, for id Search

--- services/productsService.js
+++ services/productsService.js
@@ -1,6 +1,10 @@
 const productsModel = require('../models/productsModel');
 
 const getProducts = async ({ id }) => {
+  if (id) {
+    const products = await productsModel.getProductsById(id);
+    return products;
+  }
   const products = await productsModel.getProducts();
   return products;
 };
