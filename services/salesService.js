b608d576ed1fa825070f7dd1a624b42132f8fea0
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-02 16:31:29 -0300

    feat: :sparkles: Add <getSalesById> to Service layer
    Adds conditional to check if and id has been passed to the Service layer, and calls the required model function

--- services/salesService.js
+++ services/salesService.js
@@ -2,6 +2,14 @@ const salesModel = require('../models/salesModel');
 
 const getSales = async ({ id }) => {
   let sales;
+  if (id) {
+    sales = await salesModel.getSalesById(id);
+    return sales.map((sale) => ({
+      productId: sale.product_id,
+      quantity: sale.quantity,
+      date: sale.date,
+    }));
+  }
   sales = await salesModel.getSales();
   return sales.map((sale) => ({
     saleId: sale.sale_id,
