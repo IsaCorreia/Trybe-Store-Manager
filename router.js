91ccf7d4cc4b414d6793c72fa90731db930242a7
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-03 20:38:49 -0300

    feat: :sparkles: Add POST route to </products>
    Adds POST route to access /products and imports the <productValidation> middleware

--- router.js
+++ router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const salesController = require('./controllers/salesControllers');
 const productsController = require('./controllers/productsControllers');
+const productValidation = require('./middlewares/productValidation');
 
 router.get('/products', productsController.getProducts);
 router.get('/products/:id', productsController.getProducts);
+router.post('/products', productValidation);
 
 router.get('/sales', salesController.getSales);
 router.get('/sales/:id', salesController.getSales);
