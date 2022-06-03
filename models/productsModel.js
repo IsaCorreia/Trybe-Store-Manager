0f11ffda7f46c7d2117c7beca5a43a1325c7846f
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-03 16:25:24 -0300

    feat: :sparkles: Adds ordered results in the Model layer
    Adds 'ORDER BY' to the SQL queries in the Model layer for Sales and Products

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,13 +1,15 @@
 const connection = require('./connection');
 
 const getProducts = async () => {
-  const query = 'SELECT * FROM StoreManager.products';
+  const query = 'SELECT * FROM StoreManager.products '
+  + 'ORDER BY id ASC';
   const [result] = await connection.execute(query);
   return result;
 };
 
 const getProductsById = async (id) => {
-  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ? '
+  + 'ORDER BY id ASC';
   const [[result]] = await connection.execute(query, [id]);
   return result;
 };
--- models/salesModel.js
+++ models/salesModel.js
@@ -2,7 +2,8 @@ const connection = require('./connection');
 
 const getSales = async () => {
   const query = 'SELECT * FROM StoreManager.sales_products AS sp '
-    + 'INNER JOIN StoreManager.sales AS sa ON sp.sale_id = sa.id;';
+    + 'INNER JOIN StoreManager.sales AS sa ON sp.sale_id = sa.id '
+    + 'ORDER BY id ASC;';
   const [result] = await connection.execute(query);
   return result;
 };
@@ -10,7 +11,8 @@ const getSales = async () => {
 const getSalesById = async (id) => {
   const query = 'SELECT * FROM StoreManager.sales_products AS sp '
     + 'INNER JOIN StoreManager.sales AS sa ON sp.sale_id = sa.id '
-    + 'WHERE sale_id = ?';
+    + 'WHERE sale_id = ? '
+    + 'ORDER BY id ASC';
   const [result] = await connection.execute(query, [id]);
   return result;
 };
