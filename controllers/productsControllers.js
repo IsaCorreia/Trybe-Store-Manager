b6b37c95aacd5ed9135a9794edc22a96be64d34b
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-03 17:06:41 -0300

    refactor: :recycle: Changes to <salesController> and <productsController>
    Changes from conditional structure to ternary operators for better reading and no duplicate header sending

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -2,8 +2,9 @@ const productsService = require('../services/productsService');
 
 const getProducts = async (req, res) => {
   const products = await productsService.getProducts(req.params);
-  if (products === undefined) res.status(404).json({ message: 'Product not found' });
-  res.status(200).json(products);
+  return products === undefined
+    ? res.status(404).json({ message: 'Product not found' })
+    : res.status(200).json(products);
 };
 
 module.exports = {
--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -2,8 +2,9 @@ const salesService = require('../services/salesService');
 
 const getSales = async (req, res) => {
   const sales = await salesService.getSales(req.params);
-  if (sales === undefined) res.status(404).json({ message: 'Sale not found' });
-  res.status(200).json(sales);
+  return !sales.length
+    ? res.status(404).json({ message: 'Sale not found' })
+    : res.status(200).json(sales);
 };
 
 module.exports = {
