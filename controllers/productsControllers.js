db513446671aeb06fe664bb161728f68c3e5a90a
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-03 16:29:48 -0300

    feat: :sparkles: Adds response for Not Found
    Products and Slaes controllers now return 404 status code along with appropriate message when a product/sale is not found

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -2,6 +2,7 @@ const productsService = require('../services/productsService');
 
 const getProducts = async (req, res) => {
   const products = await productsService.getProducts(req.params);
+  if (products === undefined) res.status(404).json({ message: 'Product not found' });
   res.status(200).json(products);
 };
 
--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -2,6 +2,7 @@ const salesService = require('../services/salesService');
 
 const getSales = async (req, res) => {
   const sales = await salesService.getSales(req.params);
+  if (sales === undefined) res.status(404).json({ message: 'Sale not found' });
   res.status(200).json(sales);
 };
 
