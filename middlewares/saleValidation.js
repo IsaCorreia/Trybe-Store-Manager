b97a08f5c3fd5513bf42f1f005287a0e54b3f973
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-04 02:06:56 -0300

    refactor: :technologist: Changes numbers to <httpStatusCode> in <salesValidation>

--- middlewares/saleValidation.js
+++ middlewares/saleValidation.js
@@ -1,10 +1,23 @@
+const {
+  HTTP_BAD_REQUEST_STATUS,
+  HTTP_UNPROCESSABLE_ENTITY_STATUS,
+} = require('../helpers/httpStatusCodes');
+
 const saleValidation = (req, res, next) => {
   req.body.forEach(({ quantity, productId }) => {
-    if (!productId) return res.status(400).json({ message: '"productId" is required' });
-    if (!quantity) return res.status(400).json({ message: '"quantity" is required' });
+    if (!productId) {
+      return res
+        .status(HTTP_BAD_REQUEST_STATUS)
+        .json({ message: '"productId" is required' });
+    }
+    if (!quantity) {
+      return res
+        .status(HTTP_BAD_REQUEST_STATUS)
+        .json({ message: '"quantity" is required' });
+    }
     if (quantity <= 0) {
       return res
-        .status(422)
+        .status(HTTP_UNPROCESSABLE_ENTITY_STATUS)
         .json({ message: '"quantity" must be greater than or equal to 1' });
     }
   });
