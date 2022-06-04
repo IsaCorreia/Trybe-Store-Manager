0527868daea87f9577d8bfbb47951512d63031d8
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-04 01:58:03 -0300

    refactor: :recycle: Centralized middleware importations to indexMiddleware
    Adds <productValidation> to <indexMiddleware> and exports for use in Controllers

--- middlewares/indexMiddleware.js
+++ middlewares/indexMiddleware.js
@@ -1,5 +1,7 @@
 const error = require('./error');
+const productValidation = require('./productValidation');
 
 module.exports = {
   error,
+  productValidation,
 };
--- router.js
+++ router.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const salesController = require('./controllers/salesControllers');
 const productsController = require('./controllers/productsControllers');
-const productValidation = require('./middlewares/productValidation');
+const indexMiddleware = require('./middlewares/indexMiddleware');
 
 router.get('/products', productsController.getProducts);
 router.get('/products/:id', productsController.getProducts);
-router.post('/products', productValidation);
+router.post('/products', indexMiddleware.productValidation);
 
 router.get('/sales', salesController.getSales);
 router.get('/sales/:id', salesController.getSales);
