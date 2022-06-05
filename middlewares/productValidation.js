3f8174bb77fdafc620d8671db46e4ad7a10ab3c0
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-04 23:25:24 -0300

    fix: :bug: Changes to <productValidation>
    Changes "!quantity" to "quantity === undefined", it used to return true for quantity = 0

--- middlewares/productValidation.js
+++ middlewares/productValidation.js
@@ -4,10 +4,7 @@ const {
 } = require('../helpers/httpStatusCodes');
 
 const nameFormat = /[\w]{5}/;
-const quantityFormat = /^[1-9]\d/;
-
 const validateName = (name) => nameFormat.test(name);
-const validateQuantity = (quantity) => quantityFormat.test(quantity);
 
 const productValidation = (req, res, next) => {
   const { name, quantity } = req.body;
@@ -15,7 +12,7 @@ const productValidation = (req, res, next) => {
     return res.status(HTTP_BAD_REQUEST_STATUS)
       .json({ message: '"name" is required' });
   }
-  if (!quantity) {
+  if (quantity === undefined) {
     return res.status(HTTP_BAD_REQUEST_STATUS)
       .json({ message: '"quantity" is required' });
   }
@@ -24,7 +21,7 @@ const productValidation = (req, res, next) => {
     return res.status(HTTP_UNPROCESSABLE_ENTITY_STATUS)
       .json({ message: '"name" length must be at least 5 characters long' });
   }
-  if (!validateQuantity(quantity)) {
+  if (quantity <= 0) {
     return res.status(HTTP_UNPROCESSABLE_ENTITY_STATUS)
       .json({ message: '"quantity" must be greater than or equal to 1' });
   }
