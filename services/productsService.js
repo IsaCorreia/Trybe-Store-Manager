c79a8acb0d045613e8308f67e3631e032b617702
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-06 20:15:41 -0300

    feat: :sparkles: Add <addProduct> to products SERVICE layer
    <addProduct> service layer checks if the product already exists in the database. Returns false if it does exist, and sends the data to the Model layers of not.

--- services/productsService.js
+++ services/productsService.js
@@ -10,6 +10,17 @@ const getProducts = async ({ id }) => {
   return products;
 };
 
+const addProduct = async ({ name, quantity }) => {
+  const products = await productsModel.getProducts();
+  const canCreate = !products.find((product) => product.name === name);
+  if (canCreate === true) {
+    const newProduct = await productsModel.addProduct(name, quantity);
+    return newProduct;
+  }
+  return canCreate;
+};
+
 module.exports = {
   getProducts,
+  addProduct,
 };
