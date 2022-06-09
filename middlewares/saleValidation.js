ca362b94710e4a4bff5b7ed9291b5f617235b494
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-09 20:57:45 -0300

    fix: :bug: Changes <saleValidation> works the data
    saleValidation receives an array of objects, and checks if the objects contain the specified properties, throwing and error if any of the properties are missing or invalid

--- middlewares/saleValidation.js
+++ middlewares/saleValidation.js
@@ -3,25 +3,35 @@ const {
   HTTP_UNPROCESSABLE_ENTITY_STATUS,
 } = require('../helpers/httpStatusCodes');
 
+const checkProperties = ({ productId, quantity }) => {
+  // Lançar erro personalizado, respeitando regras do ESLint:
+  // throw {status: x, message: 'y'} --> "Expected an error object to be thrown"
+  // throw new Error() --> Somente aceita STRING (Não tem como passar status + message)
+  // A solução abaixo permite criar um objeto, com um erro, e extende o Erro com um campo personalizado (no caso, status)
+  // https://stackoverflow.com/questions/53080948/generic-throw-giving-expected-an-object-to-be-thrown-lint-error
+  if (productId === undefined) {
+    throw Object.assign(new Error('"productId" is required'),
+      { status: HTTP_BAD_REQUEST_STATUS });
+  }
+  if (quantity === undefined) {
+    throw Object.assign(new Error('"quantity" is required'), 
+      { status: HTTP_BAD_REQUEST_STATUS });
+  }
+  if (quantity <= 0) {
+    throw Object.assign(
+      new Error('"quantity" must be greater than or equal to 1'),
+      { status: HTTP_UNPROCESSABLE_ENTITY_STATUS },
+    );
+  }
+};
+
 const saleValidation = (req, res, next) => {
-  req.body.forEach(({ quantity, productId }) => {
-    if (!productId) {
-      return res
-        .status(HTTP_BAD_REQUEST_STATUS)
-        .json({ message: '"productId" is required' });
-    }
-    if (!quantity) {
-      return res
-        .status(HTTP_BAD_REQUEST_STATUS)
-        .json({ message: '"quantity" is required' });
-    }
-    if (quantity <= 0) {
-      return res
-        .status(HTTP_UNPROCESSABLE_ENTITY_STATUS)
-        .json({ message: '"quantity" must be greater than or equal to 1' });
-    }
-  });
-  next();
+  try {
+    req.body.forEach((element) => checkProperties(element));
+    next();
+  } catch (e) {
+    return res.status(e.status).json({ message: e.message });
+  }
 };
 
 module.exports = saleValidation;
