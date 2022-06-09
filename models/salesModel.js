3db714a06dc09985ce229f3a7591f3195ad2e913
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-09 20:59:38 -0300

    feat: :sparkles: Add <addSale> to Model layer
    AddSale receives two numbers from the Service layer, creates a new sales registry in the database, recovers that sale id, then uses that same sale id to create a new registry in the sales_products table. Returns sale id for Service layer use

--- models/salesModel.js
+++ models/salesModel.js
@@ -17,7 +17,33 @@ const getSalesById = async (id) => {
   return result;
 };
 
+const getLastSale = async () => {
+  const getLastSaleQuery = 'SELECT * FROM StoreManager.sales '
+    + 'ORDER BY id DESC LIMIT 1;';
+  const [[{ id: saleId }]] = await connection.execute(getLastSaleQuery);
+  return saleId;
+};
+
+const addSale = async (productId, quantity) => {
+  // Add into sales table first
+  const addSalesQuery = 'INSERT INTO StoreManager.sales() VALUES();';
+  await connection.execute(addSalesQuery);
+
+  // Get last sale ID
+  const saleId = await getLastSale();
+
+  // Add into sales_products
+  const addSalesProductQuery = 'INSERT INTO StoreManager.sales_products'
+    + '(sale_id, product_id, quantity) '
+    + 'VALUES(?, ?, ?);';
+  await connection.execute(addSalesProductQuery, [saleId, productId, quantity]);
+  
+  return saleId;
+};
+
 module.exports = {
   getSales,
   getSalesById,
+  getLastSale,
+  addSale,
 };
