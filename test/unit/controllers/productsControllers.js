42f481a5f0353b8ac6d22fe7c1244d834a8992ba
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-10 02:29:36 -0300

    test: :white_check_mark: Add GET </products> Controller test

--- test/unit/controllers/productsControllers.js
+++ test/unit/controllers/productsControllers.js
@@ -1,1 +1,29 @@
-// Renomeie esse arquivo
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+const { expect } = chai;
+
+const server = require("../../../app");
+
+describe("---> Teste de Controllers: Products", () => {
+  describe("GET /products", () => {
+      let response;
+      before(async () => {
+        response = await chai.request(server).get("/products");
+      });
+
+      it("É recebido código 200",  () => {
+        expect(response).to.have.status(200);
+      });
+
+      it("É recebido a lista de produtos",  () => {
+        expect(response).to.be.an("object");
+
+        const parseResponse = JSON.parse(response.text);
+        expect(parseResponse[0]).to.have.property("id");
+        expect(parseResponse[0]).to.have.property("name");
+        expect(parseResponse[0]).to.have.property("quantity");
+      });
+  });
+
+});
