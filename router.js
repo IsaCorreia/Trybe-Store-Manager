85cc50f86dfb8dc795acbec187caf1a5bd0739b9
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-09 21:37:07 -0300

    feat: :sparkles: Add <updateSale> Controller to PUT </sales/:id> route

--- router.js
+++ router.js
@@ -12,6 +12,6 @@ router.delete('/products/:id', productsController.deleteProduct);
 router.get('/sales', salesController.getSales);
 router.get('/sales/:id', salesController.getSales);
 router.post('/sales', indexMiddleware.saleValidation, salesController.addSale);
-router.put('/sales/:id', indexMiddleware.saleValidation);
+router.put('/sales/:id', indexMiddleware.saleValidation, salesController.updateSale);
 
 module.exports = router;
