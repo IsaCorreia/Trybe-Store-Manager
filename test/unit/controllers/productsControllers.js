daf9d89dc842ca11d75ab4d8b8081d893a7899d7
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-10 04:36:03 -0300

    test: :clown_face: Add mock for GET <products> Controller test

--- test/unit/controllers/productsControllers.js
+++ test/unit/controllers/productsControllers.js
@@ -1,43 +1,75 @@
+const sinon = require("sinon");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 const { expect } = chai;
 
-const server = require("../../../app");
+const productsService = require("../../../services/productsService");
+const app = require("../../../app");
+
+const productsMock = [
+  {
+    id: 1,
+    name: "Martelo do Thor",
+    quantity: 6,
+  },
+  {
+    id: 2,
+    name: "Escudo do Capitão América",
+    quantity: 10,
+  },
+];
+
+const errorMock = { message: "Product not found" };
 
 describe("---> Teste de Controllers: Products", () => {
   describe("GET /products", () => {
-      let response;
-      before(async () => {
-        response = await chai.request(server).get("/products");
-      });
+    let request = {};
+    let response = {};
 
-      it("É recebido código 200",  () => {
-        expect(response).to.have.status(200);
-      });
+    before(async () => {
+      request.body = {};
+      sinon.stub(productsService, "getProducts").resolves(productsMock);
+      response = await chai.request(app).get("/products");
+    });
 
-      it("É recebido a lista de produtos",  () => {
-        expect(response).to.be.an("object");
+    after(() => {
+      productsService.getProducts.restore();
+    });
 
-        const parseResponse = JSON.parse(response.text);
-        expect(parseResponse[0]).to.have.property("id");
-        expect(parseResponse[0]).to.have.property("name");
-        expect(parseResponse[0]).to.have.property("quantity");
-      });
+    it("É recebido código 200", () => {
+      expect(response).to.have.status(200);
+    });
+
+    it("É recebido a lista de produtos", () => {
+      expect(response).to.be.an("object");
+
+      const parseResponse = JSON.parse(response.text);
+      expect(parseResponse[0]).to.have.property("id");
+      expect(parseResponse[0]).to.have.property("name");
+      expect(parseResponse[0]).to.have.property("quantity");
+    });
   });
 
   describe(" GET /products/:id", () => {
     describe("com ID válido", () => {
-      let response;
+      let request = {};
+      let response = {};
+
       before(async () => {
-        response = await chai.request(server).get("/products/1");
+        request.body = {};
+        sinon.stub(productsService, "getProducts").resolves(productsMock[0]);
+        response = await chai.request(app).get("/products");
+      });
+      after(() => {
+        productsService.getProducts.restore();
       });
 
-      it("É recebido código 200",  () => {
+      it("É recebido código 200", () => {
         expect(response).to.have.status(200);
       });
 
-      it("É recebido o produto selecionado",  () => {
+      it("É recebido o produto selecionado", () => {
         expect(response).to.be.an("object");
 
         const parseResponse = JSON.parse(response.text);
@@ -45,24 +77,29 @@ describe("---> Teste de Controllers: Products", () => {
         expect(parseResponse).to.have.property("name");
         expect(parseResponse).to.have.property("quantity");
       });
-    } );
+    });
     describe("com ID inválido", () => {
+      let request = {};
+      let response = {};
+
+      before(async () => {
       let response;
       before( async () => {
         response = await chai.request(server).get("/products/100");
+        response = await chai.request(app).get("/products/100");
       });
 
-      it("É recebido código 404",  () => {
+      it("É recebido código 404", () => {
         expect(response).to.have.status(404);
       });
 
-      it("É recebido a mensagem 'Product not found'",  () => {
+      it("É recebido a mensagem 'Product not found'", () => {
         expect(response).to.be.an("object");
 
         const parseResponse = JSON.parse(response.text);
         expect(parseResponse).to.have.property("message");
         expect(parseResponse.message).to.be.an.string("Product not found");
       });
-    } );
+    });
   });
 });
