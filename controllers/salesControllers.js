b9ba30bb71ea6231ebf8523af4868d1524b0ef8f
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-10 00:49:40 -0300

    refactor: :art: Changes Express Router methods from <router> to <salesController>

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -1,3 +1,5 @@
+const salesRouter = require('express').Router();
+const indexMiddleware = require('../middlewares/indexMiddleware');
 const salesService = require('../services/salesService');
 const {
   HTTP_OK_STATUS,
@@ -5,25 +7,28 @@ const {
   HTTP_CREATED_STATUS,
 } = require('../helpers/httpStatusCodes');
 
-const getSales = async (req, res) => {
+salesRouter.get('/', async (req, res) => {
+  const sales = await salesService.getSales({ id: false });
+  return !sales.length
+    ? res.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Sale not found' })
+    : res.status(HTTP_OK_STATUS).json(sales);
+});
+
+salesRouter.get('/:id', async (req, res) => {
   const sales = await salesService.getSales(req.params);
   return !sales.length
     ? res.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Sale not found' })
     : res.status(HTTP_OK_STATUS).json(sales);
-};
+});
 
-const addSale = async (req, res) => {
+salesRouter.post('/', indexMiddleware.saleValidation, async (req, res) => {
   const newSale = await salesService.addSale(req.body);
   return res.status(HTTP_CREATED_STATUS).json(newSale);
-};
+});
 
-const updateSale = async (req, res) => {
+salesRouter.put('/:id', indexMiddleware.saleValidation, async (req, res) => {
   const updatedSale = await salesService.updateSale(req.params, ...req.body);
   return res.status(HTTP_OK_STATUS).json(updatedSale);
-};
+});
 
-module.exports = {
-  getSales,
-  addSale,
-  updateSale,
-};
+module.exports = salesRouter;
--- router.js
+++ router.js
@@ -1,13 +1,8 @@
 const router = require('express').Router();
 const salesController = require('./controllers/salesControllers');
 const productsController = require('./controllers/productsControllers');
-const indexMiddleware = require('./middlewares/indexMiddleware');
 
 router.use('/products', productsController);
-
-router.get('/sales', salesController.getSales);
-router.get('/sales/:id', salesController.getSales);
-router.post('/sales', indexMiddleware.saleValidation, salesController.addSale);
-router.put('/sales/:id', indexMiddleware.saleValidation, salesController.updateSale);
+router.use('/sales', salesController);
 
 module.exports = router;
