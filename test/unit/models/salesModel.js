b3cd5fbe8a513fbd99f380bdcdb582256f8e873e
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-06-14 20:38:03 -0300

    test: :white_check_mark: Add <salesModel> <getSales> test

---
+++ test/unit/models/salesModel.js
@@ -0,0 +1,25 @@
+const sinon = require("sinon");
+const chai = require("chai");
+const { expect } = chai;
+
+const connection = require("../../../models/connection");
+const salesModel = require("../../../models/salesModel");
+const {
+  modelMocks: { getSalesMock },
+} = require("../mocks");
+
+describe("---> Teste de Model: Sales", () => {
+  describe("getSales", () => {
+    before(() => {
+      sinon.stub(connection, "execute").resolves(getSalesMock);
+    });
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it("Retorna todas as compras", async () => {
+      const result = await salesModel.getSales();
+      expect(result).to.be.an("array");
+    });
+  });
+  });
